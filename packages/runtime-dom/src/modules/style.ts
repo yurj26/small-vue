1d1644eefcc43e435be74c490dc0bf08501672ac
Yurj <1816387074@qq.com>
2022-10-31 10:44:29 +0800

    test(runtime dom): add patch style test

--- packages/runtime-dom/src/modules/style.ts
+++ packages/runtime-dom/src/modules/style.ts
@@ -1,18 +1,47 @@
+import { isArray, isString } from '@small-vue/shared'
+
 export function patchStyle(el, prev, next) {
   const style = el.style
+  const isCssString = isString(next)
 
-  if (next) {
+  if (next && !isCssString) {
     for (let key in next) {
-      style[key] = next[key]
+      setStyle(style, key, next[key])
     }
     if (prev) {
       for (let key in prev) {
         if (next[key] == null) {
-          style[key] = null
+          setStyle(style, key, '')
         }
       }
     }
   } else {
-    el.removeAttribute('style')
+    if (isCssString) {
+      if (prev !== next) {
+        style.cssText = next as string
+      }
+    } else if (prev) {
+      el.removeAttribute('style')
+    }
+  }
+}
+const importantRE = /\s*!important$/
+
+function setStyle(style, name, val) {
+  if (isArray(val)) {
+    val.forEach(v => setStyle(style, name, v))
+  } else {
+    if (val == null) val = ''
+    if (name.startsWith('--')) {
+      // custom property definition
+      style.setProperty(name, val)
+    } else {
+      if (importantRE.test(val)) {
+        // !important
+        style.setProperty(name, val.replace(importantRE, ''), 'important')
+      } else {
+        style[name] = val
+      }
+    }
   }
 }
