fef98287e4f3328ffde0598100e33ba4a6a0bc85
Yurj <1816387074@qq.com>
2022-11-15 19:30:52 +0800

    feat(renderer): add nextTick

--- packages/runtime-core/src/scheduler.ts
+++ packages/runtime-core/src/scheduler.ts
@@ -1,23 +1,48 @@
-let queue: any[] = []
-let isFlushing = false
-const reslovePromise = Promise.resolve()
+const queue: any[] = []
+const activePreFlushCbs: any = []
+
+const p = Promise.resolve()
+let isFlushPending = false
+
+export function nextTick(fn?) {
+  return fn ? p.then(fn) : p
+}
 
 export function queueJob(job) {
   if (!queue.includes(job)) {
     queue.push(job)
+    queueFlush()
   }
+}
 
-  if (!isFlushing) {
-    isFlushing = true
-    reslovePromise.then(() => {
-      isFlushing = false
-      let copy = queue.slice(0)
-      queue.length = 0
-      for (let i = 0; i < copy.length; i++) {
-        let job = copy[i]
-        job()
-      }
-      copy.length = 0
-    })
+function queueFlush() {
+  if (isFlushPending) return
+  isFlushPending = true
+  nextTick(flushJobs)
+}
+
+export function queuePreFlushCb(cb) {
+  queueCb(cb, activePreFlushCbs)
+}
+
+function queueCb(cb, activeQueue) {
+  activeQueue.push(cb)
+
+  queueFlush()
+}
+
+function flushJobs() {
+  isFlushPending = false
+
+  flushPreFlushCbs()
+  let job
+  while ((job = queue.shift())) {
+    job && job()
+  }
+}
+
+function flushPreFlushCbs() {
+  for (let i = 0; i < activePreFlushCbs.length; i++) {
+    activePreFlushCbs[i]()
   }
 }
